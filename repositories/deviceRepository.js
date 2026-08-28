e1a3da71b31ed7011278d5f2d0d3c06a057ae5a6
Ferrette <Pedro_Ferrette@steris.com>
2026-08-28 12:33:21 -0300

    clean useless comments in the code

--- repositories/deviceRepository.js
+++ repositories/deviceRepository.js
@@ -13,35 +13,6 @@ const getDevices = async (req, res) => {
   }
 };
 
-// const getUserById = async (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   console.log(id);
-//   try {
-//     const results = await pool.query(`SELECT * FROM "Users" WHERE "Id"=${id}`);
-//     res.status(200).json(results.rows);
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     //await pool.end();
-//   }
-// };
-
-// const createUser = async (req, res) => {
-//   const { name, cc, email } = req.body;
-//   try {
-//     const results = await pool.query(
-//       `INSERT INTO "Users" ("Name", "CC", "Email")
-//    VALUES ($1, $2, $3)`,
-//       [name, cc, email],
-//     );
-//     res.status(201).send(`User Added`);
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     // await pool.end();
-//   }
-// };
-
 const makeComment = async (req, res) => {
   const id = parseInt(req.params.id, 10);
 
@@ -59,18 +30,6 @@ const makeComment = async (req, res) => {
     // await pool.end();
   }
 };
-
-// const deleteUsers = async (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   try {
-//     console.log(id);
-//     await pool.query('DELETE FROM "Users" WHERE "Id" = $1', [id]);
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     // await pool.end();
-//   }
-// };
 
 const changeUser = async (req, res) => {
   const id = parseInt(req.params.id, 10);
